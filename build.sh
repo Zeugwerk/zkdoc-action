6c0ecd090c3c78892829072fb594a511803e0915
Donkey <donkey@zeugwerk.at>
2022-03-03 16:26:28 +0100

    chore: moved to operations.zeugwerk.at

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [ "$BRANCH" == "" ]; then
 fi;
 BRANCH=$(echo -n $BRANCH | tr "/" "-")
 
-curl -s -k --fail --show-error -N -G --data-urlencode "scm=$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" --data-urlencode "sha=$GITHUB_SHA" --data-urlencode "branch=$BRANCH" --data-urlencode "username=$1" --data-urlencode "password=$2" --data-urlencode "filepath=$3" --data-urlencode "working-directory=$4" --data-urlencode "method=zkdoc" https://wp13699039.server-he.de/api.php | tee response
+curl -s --fail --show-error -N -G --data-urlencode "scm=$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" --data-urlencode "sha=$GITHUB_SHA" --data-urlencode "branch=$BRANCH" --data-urlencode "username=$1" --data-urlencode "password=$2" --data-urlencode "filepath=$3" --data-urlencode "working-directory=$4" --data-urlencode "method=zkdoc" https://operations.zeugwerk.at/api.php | tee response
 status="$(tail -n1 response)"
 if [[ "$status" != *"HTTP/1.1 200"* ]]; then
     exit 1
@@ -14,7 +14,7 @@ fi
 
 ARTIFACT_MD5=`printf '%s' "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" | md5sum | awk '{print $1}'`
 ARTIFACT="$1_zkdoc_$ARTIFACT_MD5.zip"
-wget --no-check-certificate https://wp13699039.server-he.de/public/$ARTIFACT
+wget https://operations.zeugwerk.at/public/$ARTIFACT
 if [[ $? -ne 0 ]]; then
     exit 1
 fi
