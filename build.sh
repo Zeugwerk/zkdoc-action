55dd55a6ddb7f67a36fa9f145ef2112d8d07e098
Donkey <donkey@zeugwerk.at>
2022-03-03 14:07:42 +0100

    fix: temporary ignore ssl until we move to new uri

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [ "$BRANCH" == "" ]; then
 fi;
 BRANCH=$(echo -n $BRANCH | tr "/" "-")
 
-curl -s --fail --show-error -N -G --data-urlencode "scm=$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" --data-urlencode "sha=$GITHUB_SHA" --data-urlencode "branch=$BRANCH" --data-urlencode "username=$1" --data-urlencode "password=$2" --data-urlencode "filepath=$3" --data-urlencode "working-directory=$4" --data-urlencode "method=zkdoc" https://wp13699039.server-he.de/api.php | tee response
+curl -s -k --fail --show-error -N -G --data-urlencode "scm=$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" --data-urlencode "sha=$GITHUB_SHA" --data-urlencode "branch=$BRANCH" --data-urlencode "username=$1" --data-urlencode "password=$2" --data-urlencode "filepath=$3" --data-urlencode "working-directory=$4" --data-urlencode "method=zkdoc" https://wp13699039.server-he.de/api.php | tee response
 status="$(tail -n1 response)"
 if [[ "$status" != *"HTTP/1.1 200"* ]]; then
     exit 1
