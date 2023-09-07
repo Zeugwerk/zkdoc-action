966e53ca31d16e67013661c2f2b703cb8141c73e
Donkey <donkey@zeugwerk.at>
2023-09-07 20:56:24 +0200

    feat: get artifact url from response

--- build.sh
+++ build.sh
@@ -8,26 +8,24 @@ fi;
 
 curl -s --fail --show-error -N -G --data-urlencode "scm=$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" --data-urlencode "sha=$GITHUB_SHA" --data-urlencode "branch=$BRANCH" --data-urlencode "username=$1" --data-urlencode "password=$2" --data-urlencode "filepath=$3" --data-urlencode "working-directory=$4" --data-urlencode "method=zkdoc" https://operations.zeugwerk.dev/api.php | tee response
 status="$(tail -n1 response)"
+artifact="$(tail -n2 response | head -n1 | cut -d '=' -f2)"
 
 # Status is not SUCCESS and not UNSTABLE
 if [[ "$status" != *"HTTP/1.1 201"* ]] && [[ "$status" != *"HTTP/1.1 202"* ]]; then
     exit 1
 fi
 
-# We got and artifact that we can extract
+# We got an artifact that we can extract
 if [[ "$status" = *"HTTP/1.1 202"* ]]; then
-    ARTIFACT_MD5=`printf '%s' "ZKDOC_$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/$GITHUB_SHA" | md5sum | awk '{print $1}'`
-    ARTIFACT="$ARTIFACT_MD5.zip"
-    wget -q https://operations.zeugwerk.dev/public/$ARTIFACT
+    wget -q $artifact -o 'artifact.zip'
     if [[ $? -ne 0 ]]; then
         exit 202
     fi
     
     # return code 0 means no errors
     # return code 1 means there was an error or warning, but processing was successful anyway
-    unzip -q -o $ARTIFACT
+    unzip -q -o 'artifact.zip'
     if [[ $? -gt 1 ]]; then
         exit 202
     fi
 fi
-
