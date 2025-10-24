6d9bf0a800ce904d60264e24821eac6509151a67
Donkey <donkey@zeugwerk.at>
2025-10-24 08:14:40 +0200

    fix: use last sha on origin

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM python:3.8
 COPY build.sh /build.sh
+RUN apt-get update && apt-get install -y jq
 RUN chmod +x /build.sh
 WORKDIR /
 ENTRYPOINT ["/build.sh"]
--- build.sh
+++ build.sh
@@ -5,6 +5,14 @@ if [ "$BRANCH" == "" ]; then
     BRANCH=$(echo $GITHUB_REF | sed 's/refs\/heads\///');
 fi;
 
+SHA="$GITHUB_SHA"
+
+if [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then
+    SHA=$(jq -r .pull_request.head.sha "$GITHUB_EVENT_PATH")
+fi
+
+echo "Using commit SHA: $SHA"
+
 echo "Requesting build ..."
 
 curl -s --show-error -N \
